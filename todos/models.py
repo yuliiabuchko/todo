6c05af0350b8a60ebfec1597834936cb3231e7d5
Yuliia Buchko <yuliiabuchko@gmail.com>
2020-01-18 21:31:25 +0100

    Add Status

--- todos/models.py
+++ todos/models.py
@@ -10,3 +10,29 @@ class Task(models.Model):
 
     def __str__(self):
         return self.task
+
+
+class Status(models.Model):
+    SELECTED = 'S'
+    HALF_DONE = 'H'
+    DONE = 'D'
+    MOVED = 'M'
+    CANCELED = 'C'
+
+    DAY_PROPERTY = (
+        (SELECTED, 'Selected'),
+        (HALF_DONE, 'Half-Done'),
+        (DONE, 'Done'),
+        (MOVED, 'Moved'),
+        (CANCELED, 'Canceled'),
+    )
+
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    day = models.DateField()
+    result = models.CharField(max_length=1, choices=DAY_PROPERTY)
+
+    def _str_(self):
+        return self.task
+
+    class Meta:
+        unique_together = (("task", "day"),)
