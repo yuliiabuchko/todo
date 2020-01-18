89e7bca945db073207f2c3599b79b8e594f44295
Yuliia Buchko <yuliiabuchko@gmail.com>
2020-01-18 22:43:55 +0100

    Add start_day

--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -6,7 +8,10 @@ class Task(models.Model):
     task = models.CharField(max_length=255)
     owner = models.ForeignKey(
         User, related_name="todos", on_delete=models.CASCADE, null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    # TODO when change to DateField error in frontend occurs
+    # created_at = models.DateField(auto_now=False, auto_now_add=False, default=datetime.date.today)
+    start_day = models.DateField(auto_now_add=False, auto_now=False, null=True)
 
     def __str__(self):
         return self.task
