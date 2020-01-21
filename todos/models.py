e2f0bfce9b5da94c48a684e061d75107cba28f3e
DominikWolek <dominik.artur.wolek@gmail.com>
2020-01-21 17:50:50 +0100

    Add Entry, Statistic, Event models

--- todos/models.py
+++ todos/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from datetime import date
 
 class Todo(models.Model):
     task = models.CharField(max_length=255)
@@ -10,3 +10,50 @@ class Todo(models.Model):
 
     def __str__(self):
         return self.task
+
+class Statistic(models.Model):
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE, null=False
+    )
+    name = models.CharField(max_length=255)
+    HOURS = "h"
+    MINUTES = "min"
+    PIECES = "pcs"
+    UNITS_CHOISES = [
+        (HOURS, "HOURS"),
+        (PIECES, "PIECES"),
+        (MINUTES, "MINUTES")
+    ]
+    unit = models.CharField(
+        choices = UNITS_CHOISES,
+        default = HOURS,
+        max_length = 3
+    )
+
+    def __str__(self):
+        return self.name
+
+class Entry(models.Model):
+    statistic = models.ForeignKey(
+        Statistic, on_delete=models.CASCADE, null=False
+    )
+    value = models.FloatField()
+    date = models.DateField(
+        auto_now=False, auto_now_add=False, default=date.today
+    )
+
+    def __float__(self):
+        return self.value
+
+    def __int__(self):
+        return float(self.__float__())
+
+class Event(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255*10)
+    date = models.DateField(
+        auto_now=False, auto_now_add=False, default=date.today
+    )
+
+    def __str__(self):
+        return self.name
