fb8a534edabc8b6a5348480afeee98588bc50de9
Yuliia Buchko <yuliiabuchko@gmail.com>
2020-01-21 18:19:25 +0100

    Add TaskStatusSerializer

--- todos/api/serializers.py
+++ todos/api/serializers.py
@@ -1,9 +1,23 @@
 from rest_framework import serializers
 
-from todos.models import Task
+from todos.models import Task, Status
 
 
 class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = '__all__'
+
+
+class StatusSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Status
+        fields = '__all__'
+
+
+class TaskStatusSerializer:
+    statuses = StatusSerializer(many=True)
+
     class Meta:
         model = Task
         fields = '__all__'
