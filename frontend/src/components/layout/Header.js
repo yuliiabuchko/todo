b3f104744976986a2257d186c7ba2a277130da52
DominikWolek <dominik.artur.wolek@gmail.com>
2020-02-02 11:21:38 +0100

    Add dropdown menu

--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -2,44 +2,41 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { logout } from '../../actions/auth';
+import Dropdown from 'react-bootstrap/Dropdown'
+import DropdownButton from 'react-bootstrap/DropdownButton'
 
 class Header extends Component {
   render() {
     const { user, isAuthenticated } = this.props.auth;
 
     const userLinks = (
-      <div className='right menu'>
-        <div className='ui simple dropdown item'>
-          {user ? user.username : ''}
-          <i className='dropdown icon' />
-          <div className='menu'>
-            <a onClick={this.props.logout} className='item'>
-              Logout
-            </a>
-          </div>
-        </div>
-      </div>
+      <>
+        <DropdownButton variant="secondary" id="user-dropdown-button" title={user ? user.username : 'user'}>
+          <Dropdown.Item as="button"><a onClick={this.props.logout}>Logout</a></Dropdown.Item>
+        </DropdownButton>
+        <DropdownButton variant="secondary" id="planner-dropdown-button" title="Planner">
+          <Dropdown.Item href="#weeks">Weeks</Dropdown.Item>
+          <Dropdown.Item href="#tasks">Tasks</Dropdown.Item>
+          <Dropdown.Item href="#events">Events</Dropdown.Item>
+        </DropdownButton>
+      </>
     );
 
     const guestLinks = (
-      <div className='right menu'>
-        <Link to='/register' className='item'>
-          Sign Up
-        </Link>
-        <Link to='/login' className='item'>
-          Login
-        </Link>
-      </div>
+      <Dropdown>
+        <Dropdown.Toggle variant="secondary" id="dropdown-secondary">
+          Menu
+        </Dropdown.Toggle>
+
+        <Dropdown.Menu>
+          <Dropdown.Item href="/register">Register</Dropdown.Item>
+          <Dropdown.Item href="/login">Login</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
     );
 
     return (
       <div className='ui inverted menu' style={{ borderRadius: '0' }}>
-        <Link to='/' className='header item'>
-          TodoCRUD
-        </Link>
-        <Link to='/' className='item'>
-          Home
-        </Link>
         {isAuthenticated ? userLinks : guestLinks}
       </div>
     );
