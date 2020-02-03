bf6414dbda1926d7c68b9d7df5980cfb373c235e
DominikWolek <dominik.artur.wolek@gmail.com>
2020-02-04 00:47:15 +0100

    Add parallax for Events and Tasks

--- frontend/src/components/todos/TodoDashboard.js
+++ frontend/src/components/todos/TodoDashboard.js
@@ -2,15 +2,22 @@ import React, { Component } from 'react';
 import TodoCreate from './TodoCreate';
 import TodoList from './TodoList';
 import WeekList from "./WeekList";
+import {Parallax, ParallaxLayer} from 'react-spring/renderprops-addons'
 
 class TodoDashboard extends Component {
   render() {
     return (
-      <div className='ui container'>
-        <TodoCreate />
-        <TodoList />
-        {/*<WeekList />*/}
-      </div>
+            <Parallax pages={2} style={{ backgroundColor: '#f9dfd5' }} ref={ref => (this.parallax = ref)}>
+              <ParallaxLayer offset={0} speed={0} style={{ backgroundColor: '#f9dfd5' }} />
+              <ParallaxLayer offset={0.6} speed={0.5} style={{ backgroundColor: '#f9f1d5' }} />
+              <ParallaxLayer offset={1} speed={0.5} style={{ backgroundColor: '#eff9d5' }} />
+              <ParallaxLayer offset={0.3} speed={-1}>
+                <TodoList />
+              </ParallaxLayer>
+              <ParallaxLayer offset={0} speed={0.2}>
+                <TodoCreate />
+              </ParallaxLayer>
+            </Parallax>
     );
   }
 }
